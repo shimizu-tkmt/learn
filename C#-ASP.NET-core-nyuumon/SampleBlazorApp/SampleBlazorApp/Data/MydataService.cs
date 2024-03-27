032771635eefb60390b6eafd4b35c5418ded68d4
tk32 <shimizu.tkmt@gmail.com>
2024-03-27 17:17:14 +0900

    4-18

---
+++ C#-ASP.NET-core-nyuumon/SampleBlazorApp/SampleBlazorApp/Data/MydataService.cs
@@ -0,0 +1,17 @@
+﻿namespace SampleBlazorApp.Data;
+
+public class MydataService
+{
+    private List<Mydata> datos = new()
+    {
+        new Mydata("Taro", "hoge", "taro@yamada"),
+        new Mydata("Hanako", "foo", "hanako@flower"),
+        new Mydata("Sachiko", "bar", "sachiko@happy")
+    };
+
+    public Task<Mydata> GetMydataAsync(int n)
+    {
+        int num = n < 0 ? 0 : n > datos.Count ? datos.Count : n;
+        return Task.FromResult(datos[n]);
+    }
+}
