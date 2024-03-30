9078afdb3cf44536091525f786725ca0f8e48415
tk32 <shimizu.tkmt@gmail.com>
2024-03-30 16:37:12 +0900

    5-39 MVCアプリケーションのデータベース利用

---
+++ C#-ASP.NET-core-nyuumon/SampleMVCApp/SampleMVCApp/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleMVCApp.Models;
+
+public class Person
+{
+    public int PersonID { get; set; }
+    public string Name { get; set; }
+    public string? Mail { get; set; }
+    public int Age { get; set; }
+}
