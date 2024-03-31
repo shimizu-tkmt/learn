3c75e7b28ef25188094f6e4c3c995d5fae28b4b2
tk32 <shimizu.tkmt@gmail.com>
2024-03-31 20:29:54 +0900

    6-40 Personで独自の検証を行う

--- C#-ASP.NET-core-nyuumon/SampleRazorApp/SampleRazorApp/Models/Person.cs
+++ C#-ASP.NET-core-nyuumon/SampleRazorApp/SampleRazorApp/Models/Person.cs
@@ -1,20 +1,35 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace SampleRazorApp.Models;
 
-public class Person
+public class Person: IValidatableObject
 {
     public int PersonId {  get; set; }
 
     [Display(Name="名前")]
-    [Required(ErrorMessage = "必須項目です。")]
     public string Name { get; set; }
 
     [Display(Name="メールアドレス")]
-    [EmailAddress(ErrorMessage = "メールアドレスの形式が正しくありません。")]
     public string? Mail { get; set; }
 
     [Display(Name="年齢")]
-    [Range(0, 200, ErrorMessage ="ゼロ以上200以下の値にしてください。")]
     public int Age { get; set; }
+
+    public IEnumerable<ValidationResult>
+        Validate(ValidationContext validationContext)
+    {
+        if(Name == null)
+        {
+            yield return new ValidationResult("名前は必須項目です。");
+        }
+        if(Mail != null && !Regex.IsMatch(Mail, "[a-zA-Z0-9.+-_%]+@[a-zA-Z0-9.-]+"))
+        {
+            yield return new ValidationResult("メールアドレスが正しい形式ではありません。");
+        }
+        if(Age < 0)
+        {
+            yield return new ValidationResult("年齢はマイナスにできません。");
+         }
+    }
 }
