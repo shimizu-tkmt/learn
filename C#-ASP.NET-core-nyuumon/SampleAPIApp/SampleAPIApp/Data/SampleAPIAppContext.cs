a1551a5702a250de471b5672bdc5a14d7ac1ba7e
tk32 <shimizu.tkmt@gmail.com>
2024-04-08 16:35:38 +0900

    7-7 Web APIアプリケーション

---
+++ C#-ASP.NET-core-nyuumon/SampleAPIApp/SampleAPIApp/Data/SampleAPIAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SampleAPIApp.Models;
+
+namespace SampleAPIApp.Data
+{
+    public class SampleAPIAppContext : DbContext
+    {
+        public SampleAPIAppContext (DbContextOptions<SampleAPIAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SampleAPIApp.Models.Product> Product { get; set; } = default!;
+    }
+}
